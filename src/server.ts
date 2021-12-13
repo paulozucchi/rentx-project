8f02c93c933ea9894428b5252d127523acd28184
Paulo Zucchi <zucchi.paulo@gmail.com>
2021-12-13 16:12:09 -0300

    Work initial

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   return response.send("ok");
 });
+
 const port = 3333;
+
 app.listen(port, () => console.log(`Server is runing on port ${port}`));

eafdb5730bca577ccd3162b0b64fd416029f504c
Paulo Zucchi <zucchi.paulo@gmail.com>
2022-01-30 10:35:01 -0300

    Working on categories import

--- src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
+++ src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
@@ -1,6 +1,15 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
 class ImportCategoriesUseCase {
   execute(file: any): void {
-    console.log(file);
+    const parseFile = parse();
+
+    const stream = fs.createReadStream(file.path).pipe(parseFile);
+
+    parseFile.on("data", async (line: any) => {
+      console.log(line);
+    });
   }
 }
 export { ImportCategoriesUseCase };
